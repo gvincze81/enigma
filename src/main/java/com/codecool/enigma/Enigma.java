f86fe0e2f43c44f01df340412c4baf976cfcd120
Vincze Gábor <cantataprofana1930@gmail.com>
2021-09-06 15:26:48 +0200

    implement translation from english to morse

--- src/main/java/com/codecool/enigma/Enigma.java
+++ src/main/java/com/codecool/enigma/Enigma.java
@@ -27,7 +27,7 @@ class Enigma {
 
             if(cipher == null)
                 throw new EnigmaException("error");
-            String encodedMessage = cipher.decrypt(readFile(argsParser.getFile()));
+            String encodedMessage = cipher.encrypt(readFile(argsParser.getFile()));
             System.out.println(encodedMessage);
 
         } catch(EnigmaException e){
--- src/main/java/com/codecool/enigma/MorseCode.java
+++ src/main/java/com/codecool/enigma/MorseCode.java
@@ -1,10 +1,69 @@
 package com.codecool.enigma;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class MorseCode implements Cipher {
+
+    public Map<Character, String> toMorse(){
+        Map<Character, String> morse = new HashMap<>();
+        morse.put('a', ".-");
+        morse.put('b', "-...");
+        morse.put('c',  "-.-.");
+        morse.put('d',  "-..");
+        morse.put('e',    ".");
+        morse.put('f', "..-.");
+        morse.put('g',  "--.");
+        morse.put('h', "....");
+        morse.put('i',   "..");
+        morse.put('j', ".---");
+        morse.put('k',   "-.");
+        morse.put('l', ".-..");
+        morse.put('m',   "--");
+        morse.put('n',   "-.");
+        morse.put('o',  "---");
+        morse.put('p', ".--.");
+        morse.put('q', "--.-");
+        morse.put('r', ".-.");
+        morse.put('s',  "...");
+        morse.put('t',   "-");
+        morse.put('u',  "..-");
+        morse.put('v', "...-");
+        morse.put('w',  ".--");
+        morse.put('x', "-..-");
+        morse.put('y', "-.--");
+        morse.put('z', "--..");
+        morse.put('1', ".----");
+        morse.put('2',"..---");
+        morse.put('3', "...--");
+        morse.put('4', "....-");
+        morse.put('5', ".....");
+        morse.put('6', "-....");
+        morse.put('7', "--...");
+        morse.put('8', "---..");
+        morse.put('9', "----.");
+        morse.put('0', "-----");
+
+        return morse;
+    }
 
     @Override
     public String encrypt(String message) {
-        return null;
+        message = message.toLowerCase();
+        Map<Character, String> englishToMorse = toMorse();
+        message = message.replaceAll(" +", "/");
+        char[] letters = message.toCharArray();
+
+        String encoded = "";
+        for(char ch: letters){
+            if(Character.isLetterOrDigit(ch)){
+                encoded = encoded.concat(englishToMorse.get(ch)) + " ";
+            }else if(ch == '/') {
+                encoded = encoded.concat("/ ");
+            }
+
+        }
+        return encoded;
     }
 
     @Override
